ec1261f34ea7f42026928cda80251923b1b203b1
koen <koen967@hotmail.com>
2017-03-31 10:37:30 +0200

    Deze DatabaseConnection.class is waarschijnlijk beter

--- src/main/java/oose/dea/dataAccess/databaseConnection/DatabaseConnection.java
+++ src/main/java/oose/dea/dataAccess/databaseConnection/DatabaseConnection.java
@@ -11,7 +11,7 @@ import java.util.Properties;
 
 public class DatabaseConnection {
 
-    public Connection getConnection(){
+    public Connection getConnection() {
         ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
 
         InputStream resourceAsStream = contextClassLoader.getResourceAsStream("JDBC.properties");
@@ -19,24 +19,53 @@ public class DatabaseConnection {
         Properties props = new Properties();
         Connection conn = null;
 
+
         try {
             props.load(resourceAsStream);
 
-            String connectionString = props.getProperty("DB_URL") + ";user=" + props.getProperty("DB_USERNAME") + ";password=" + props.getProperty("DB_PASSWORD") + ";";
             Class.forName(props.getProperty("DB_DRIVER_CLASS"));
-            conn = DriverManager.getConnection(connectionString);
 
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            conn = DriverManager.getConnection(props.getProperty("DB_URL"),
+                    props.getProperty("DB_USERNAME"),
+                    props.getProperty("DB_PASSWORD"));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return conn;
     }
+
+//    public Connection getConnection(){
+//        ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+//
+//        InputStream resourceAsStream = contextClassLoader.getResourceAsStream("JDBC.properties");
+//
+//        Properties props = new Properties();
+//        Connection conn = null;
+//
+//        try {
+//            props.load(resourceAsStream);
+//
+//            String connectionString = props.getProperty("DB_URL") + ";user=" + props.getProperty("DB_USERNAME") + ";password=" + props.getProperty("DB_PASSWORD") + ";";
+//            Class.forName(props.getProperty("DB_DRIVER_CLASS"));
+//            conn = DriverManager.getConnection(connectionString);
+//
+//        } catch (SQLException ex) {
+//            ex.printStackTrace();
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return conn;
+//    }
 
     public static void main(String[] args) {
         DatabaseConnection connection = new DatabaseConnection();
